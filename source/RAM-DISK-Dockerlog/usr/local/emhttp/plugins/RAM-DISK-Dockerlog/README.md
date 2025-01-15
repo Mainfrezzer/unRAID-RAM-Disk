1e92244304cf5d229e2733d2c6aa86f7585cafa4
Mainfrezzer <35963833+Mainfrezzer@users.noreply.github.com>
2025-01-15 09:45:04 +0100

    init

---
+++ source/RAM-DISK-Dockerlog/usr/local/emhttp/plugins/RAM-DISK-Dockerlog/README.md
@@ -0,0 +1,3 @@
+**RAM-Disk for Docker logs**
+
+This plugin reduces the wear on your SSD by moving the docker status and log files into your RAM. Thanks to the work of MGutt.
---
+++ source/RAM-DISK-Dockerlog/usr/local/emhttp/plugins/RAM-DISK-Dockerlog/php/RAM-DiskSettings.php
@@ -0,0 +1,119 @@
+<?php
+$configFilePath = '/boot/config/plugins/RAM-DISK-Dockerlog/settings.cfg';
+$modcheck = "/usr/local/emhttp/plugins/RAM-DISK-Dockerlog/modified";
+if (!file_exists($configFilePath)) {
+    die("Configuration file not found.");
+}
+
+function readSetting($file) {
+    $content = file_get_contents($file);
+    return trim($content);
+}
+
+function writeSetting($file, $value) {
+    file_put_contents($file, $value);
+    shell_exec('value=$(cat /boot/config/plugins/RAM-DISK-Dockerlog/settings.cfg) && sed -i "s/\\\$sync_interval_minutes=[0-9]\\+;/\\\$sync_interval_minutes="$value";/g" /usr/local/emhttp/plugins/dynamix/scripts/monitor');
+}
+
+$settingValue = readSetting($configFilePath);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $userInput = isset($_POST['setting']) ? intval($_POST['setting']) : 0;
+
+    if ($userInput >= 0 && $userInput <= 60) {
+        writeSetting($configFilePath, $userInput);
+        $settingValue = readSetting($configFilePath);
+        $message = "Setting updated successfully.";
+    } else {
+        $message = "Please enter a value between 0 and 60.";
+    }
+}
+
+
+function getCaseContentForVersion($filePath, $version) {
+    $content = file($filePath);
+    $capturing = false;
+    $caseContent = '';
+
+    foreach ($content as $line) {
+        if (strpos($line, "$version)") !== false) {
+            $capturing = true;
+	    continue;
+        }
+
+        if ($capturing) {
+		if (strpos($line, ';;') == false) {
+            $caseContent .= $line;
+		}
+            if (strpos($line, ';;') !== false) {
+                break; // Exit the loop
+            }
+        }
+    }
+
+    return trim($caseContent);
+}
+
+$filePath = '/usr/local/emhttp/plugins/RAM-DISK-Dockerlog/event/starting';
+
+$versionToDisplay = trim(shell_exec('source /etc/unraid-version && echo $version'));
+
+$caseContent = getCaseContentForVersion($filePath, $versionToDisplay);
+
+function check_file_exists($modcheck) {
+    if (file_exists($modcheck)) {
+        // File exists
+        echo '<div style="text-align: left; margin-top: 1em;">
+                <span style="margin-left: 0em;">Script was executed</span>
+                <input type="checkbox" checked style="accent-color: green; pointer-events: none;"/> 
+              </div>';
+    } else {
+        // File does not exist
+        echo '<div style="text-align: left; margin-top: 1em;">
+                <span style="margin-left: 0em;">Modifications have not been applied</span>
+                <span style="color: red; font-size: 1.2em;">&#10006;</span>
+              </div>';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAM-Disk for Docker settings</title>
+</head>
+<body>
+    <h1>RAM-Disk Backup Interval Setting</h1>
+    <?php if (isset($message)): ?>
+        <p><strong><?php echo htmlspecialchars($message); ?></strong></p>
+    <?php endif; ?>
+<div style="width:49%; float:left; border: 0em solid black;">
+The interval is set in minutes (0-60)
+    <form method="post" action="">
+        <!-- debug <label for="setting">Current Value: <?php echo htmlspecialchars($settingValue); ?></label> --><br>
+        <input type="number" id="setting" name="setting" min="0" max="60" value="<?php echo htmlspecialchars($settingValue); ?>" required>
+        <input type="submit" value="Save">
+    </form>
+</div>
+<div style="width: 49%; float:right; border: 0em solid black;">
+<blockquote class="inline_help" style="display: block; align=right">
+<p>This Plugin runs the script below only once. You can see the status, if it ran or not, on the left.<br/></p>
+<p>If you install this for the <b>first time</b> or you updated the plugin to support your current UnRaid version, you can simply <b>stop and start the array</b> to use it. Alternatively perfom a <b>reboot.</b></p>
+<p><b style="color: #ff0000;">ATTENTION:</b><b> To remove the script, remove the plugin and perform a reboot.</b></p>
+<p>There are multiple checks to ensure nothing breaks, you can update to a new version or downgrade to any version without issue</p>
+<p>If you see "No script found for this version", this means the plugin has no support for your current UnRaid version. An update might change that.</p>
+<p>This is only possible thanks to MGutt and his script. <a href="https://forums.unraid.net/topic/136087-ram-disk-for-docker-statuslog-files" target="_blank">https://forums.unraid.net/topic/136087-ram-disk-for-docker-statuslog-files</a><p>
+</blockquote>
+</div>
+
+<p>&nbsp;</p>
+<div style="width:99%; float:left; border: 0em solid black;">
+    <h2>Contents for Version: <?php echo htmlspecialchars($versionToDisplay); ?></h2>
+    <p><?php check_file_exists($modcheck); ?></p>
+    <pre><?php echo htmlspecialchars($caseContent ?: 'No script found for this version'); ?></pre>
+</div>
+</body>
+</html>
